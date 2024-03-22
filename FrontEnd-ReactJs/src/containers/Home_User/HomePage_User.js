8a08576b8ba0688663e622d4e9d74cda02ee3220
ngocthach0104 <thach8ahamtri@gmail.com>
2024-03-22 17:11:42 +0700

    delete

--- FrontEnd-ReactJs/src/containers/Home_User/HomePage_User.js
+++ FrontEnd-ReactJs/src/containers/Home_User/HomePage_User.js
@@ -6,22 +6,16 @@ import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min.js';
 import { path } from '../../utils'
 import { Col, Pagination, Row, Spin } from 'antd';
 
-
 // scss
 import './HomePage_User.scss'
 
 // import giao diện của HomeUser
-import Header_Top from './Header/Header_Top/Header_HomePage_User.js';
-import Header_Logo from './Header/Header_Logo/Header_Logo.js'
-import Banner from './Banner/Banner';
 import FlashSale from './FlashSale/FlashSale.js';
 import Brand from './Brand/Brand.js';
-
 import Laptop from './DoanhMuc/Laptop/index.js';
 import ManHinh from './DoanhMuc/ManHinh/index.js';
 import Gear from './DoanhMuc/Gear/index.js';
 
-import DoanhMuc from './DoanhMuc/index.js';
 // import All_Products from './SanPham/All_Products.js';
 import Footer from './Footer/Footer.js';
 import SaleOff from './SaleOff/index.js';
@@ -29,18 +23,16 @@ import Filter  from '../../components/Filter';
 import AllProduct from './SanPham/index.js';
 
 class HomePage_User extends Component {
-
+  
     render() {
         const { systemMenuPath, isLoggedIn } = this.props;
-
-
         return (
             <>
                 <div className='homepage'>
                     {/* <Header_Top /> */}
                     {/* <Header_Logo /> */}
 
-                    <div className="">
+                    <div className="Banner">
                         <SaleOff />
                         <div className="filter-wrapper trans-center container ">
                             <Filter />
@@ -48,11 +40,6 @@ class HomePage_User extends Component {
                     </div>
 
                     <Row className="container">
-
-                      
-                        {/* <Col span={24} >
-                            <Banner />
-                        </Col> */}
 
                         {/* flash sale */}
                         <Col span={24} className="m-b-32 hp-01">
